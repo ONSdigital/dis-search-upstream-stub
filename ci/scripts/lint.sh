d816218fe6f4b60f4faa9d5ddc6fb4e73f742eb0
oluwawunmi <soluwawunmi@gmail.com>
2024-11-05 10:33:09 +0000

    Add validate spec to ci

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
 pushd dis-search-upstream-stub
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
   make lint
+  npm install -g @redocly/cli
+  make validate-specification
 popd
