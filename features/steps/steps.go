dd82a3c91156e409bbd500c018dac92fa9018210
cookel2 <eleanor.cook@ons.gov.uk>
2024-11-06 19:03:59 +0000

    remove helloworld feature

--- features/steps/steps.go
+++ features/steps/steps.go
@@ -15,17 +15,7 @@ var expectedResponseBody []byte
 func (c *Component) RegisterSteps(ctx *godog.ScenarioContext) {
 	c.apiFeature.RegisterSteps(ctx)
 
-	ctx.Step(`^I should receive a hello-world response$`, c.iShouldReceiveAHelloworldResponse)
 	ctx.Step(`^I should receive a list of resources$`, c.iShouldReceiveAListOfResources)
-}
-
-func (c *Component) iShouldReceiveAHelloworldResponse() error {
-	responseBody := c.apiFeature.HTTPResponse.Body
-	body, _ := io.ReadAll(responseBody)
-
-	assert.Equal(c, `{"message":"Hello, World!"}`, strings.TrimSpace(string(body)))
-
-	return c.StepError()
 }
 
 func (c *Component) iShouldReceiveAListOfResources() error {
