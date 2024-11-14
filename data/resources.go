bbb9bdbd6014644d868e58963b6a755ae13e9dca
cookel2 <eleanor.cook@ons.gov.uk>
2024-11-14 16:29:36 +0000

    put json files in new directory and read them into a list

--- data/resources.go
+++ data/resources.go
@@ -2,12 +2,27 @@ package data
 
 import (
 	"context"
-	"time"
-
-	"github.com/ONSdigital/log.go/v2/log"
-
+	"embed"
+	"encoding/json"
+	"fmt"
 	"github.com/ONSdigital/dis-search-upstream-stub/models"
+	"github.com/ONSdigital/log.go/v2/log"
 )
+
+// //go:embed standard_resource_1.json
+// var resource1 []byte
+//
+// //go:embed standard_resource_2.json
+// var resource2 []byte
+//
+// //go:embed standard_resource_3.json
+// var resource3 []byte
+
+////go:embed folder/*.hash
+//var folder embed.FS
+
+//go:embed json_files/*.json
+var jsonFiles embed.FS
 
 // GetResources retrieves all the resources from the collection
 func (r *ResourceStore) GetResources(ctx context.Context, option Options) (*models.Resources, error) {
@@ -22,16 +37,17 @@ func (r *ResourceStore) GetResources(ctx context.Context, option Options) (*mode
 	}
 
 	// create and populate resourcesList
-	resourceList := make([]interface{}, numResources)
-	resourceList, err = r.populateResourceList(resourceList)
+	items := make([]interface{}, numResources)
+	// items, err = r.populateResourceList(items)
+	items, err = r.populateItems(ctx, items, numResources)
 	if err != nil {
 		log.Error(ctx, "failed to populate resources list", err, logData)
 		return nil, err
 	}
 
 	resources := &models.Resources{
-		Count:      len(resourceList),
-		Items:      resourceList,
+		Count:      len(items),
+		Items:      items,
 		Limit:      option.Limit,
 		Offset:     option.Offset,
 		TotalCount: numResources,
@@ -42,55 +58,85 @@ func (r *ResourceStore) GetResources(ctx context.Context, option Options) (*mode
 
 // getResourcesCount returns the total number of resources stored
 func (r *ResourceStore) getResourcesCount() (int, error) {
-	return 2, nil
+	return 3, nil
 }
 
-func (r *ResourceStore) populateResourceList(resourceList []interface{}) ([]interface{}, error) {
-	topics1 := []string{"a", "b", "c", "d"}
-	topics2 := []string{"a", "b", "e", "f"}
-	dateChanges := []string{"a change_notice", "a previous_date"}
-	tempResource1 := models.Standard{
-		URI:             "/a/temp/uri",
-		URIOld:          "/an/old/uri",
-		ContentType:     "api_dataset_landing_page",
-		CDID:            "ASELECTIONOFNUMBERSANDLETTERS123",
-		DatasetID:       "ASELECTIONOFNUMBERSANDLETTERS456",
-		Edition:         "a temporary edition",
-		MetaDescription: "a temporary description",
-		ReleaseDate:     time.Time{}.UTC(),
-		Summary:         "a temporary summary",
-		Title:           "a temporary title",
-		Topics:          topics1,
-		Language:        "string",
-		Survey:          "string",
-		CanonicalTopic:  "string",
+//	func (r *ResourceStore) populateResourceList(items []interface{}) ([]interface{}, error) {
+//		topics1 := []string{"a", "b", "c", "d"}
+//		topics2 := []string{"a", "b", "e", "f"}
+//		dateChanges := []string{"a change_notice", "a previous_date"}
+//		tempResource1 := models.Standard{
+//			URI:             "/a/temp/uri",
+//			URIOld:          "/an/old/uri",
+//			ContentType:     "api_dataset_landing_page",
+//			CDID:            "ASELECTIONOFNUMBERSANDLETTERS123",
+//			DatasetID:       "ASELECTIONOFNUMBERSANDLETTERS456",
+//			Edition:         "a temporary edition",
+//			MetaDescription: "a temporary description",
+//			ReleaseDate:     time.Time{}.UTC(),
+//			Summary:         "a temporary summary",
+//			Title:           "a temporary title",
+//			Topics:          topics1,
+//			Language:        "string",
+//			Survey:          "string",
+//			CanonicalTopic:  "string",
+//		}
+//
+//		items[0] = tempResource1
+//
+//		tempResource2 := models.Release{
+//			URI:             "/another/temp/uri",
+//			URIOld:          "/another/old/uri",
+//			ContentType:     "api_dataset_landing_page",
+//			CDID:            "ASELECTIONOFNUMBERSANDLETTERS789",
+//			DatasetID:       "ASELECTIONOFNUMBERSANDLETTERS101112",
+//			Edition:         "another temporary edition",
+//			MetaDescription: "another temporary description",
+//			ReleaseDate:     time.Time{}.UTC(),
+//			Summary:         "another temporary summary",
+//			Title:           "another temporary title",
+//			Topics:          topics2,
+//			Language:        "string",
+//			Survey:          "string",
+//			CanonicalTopic:  "string",
+//			Cancelled:       true,
+//			Finalised:       true,
+//			Published:       true,
+//			DateChanges:     dateChanges,
+//			ProvisionalDate: "October-November 2024",
+//		}
+//
+//		items[1] = tempResource2
+//
+//		return items, nil
+//	}
+func (r *ResourceStore) populateItems(ctx context.Context, items []interface{}, numItems int) ([]interface{}, error) {
+	// Get a list of JSON files in the embedded 'json_files' directory.
+	files, err := jsonFiles.ReadDir("json_files")
+	if err != nil {
+		log.Fatal(ctx, "Failed to read json_files directory", err)
 	}
 
-	resourceList[0] = tempResource1
+	// Loop through files, read, and unmarshal each JSON file into Go structs.
+	for i := 0; i < numItems; i++ {
+		//startIndex := numItems - 1
+		//for i := startIndex; i >= 0; i-- {
+		file := files[i]
+		fmt.Println("Now reading file: " + file.Name())
+		fileBytes, err := jsonFiles.ReadFile("json_files/" + file.Name())
+		if err != nil {
+			log.Fatal(ctx, "Failed to read file: "+file.Name(), err)
+		}
 
-	tempResource2 := models.Release{
-		URI:             "/another/temp/uri",
-		URIOld:          "/another/old/uri",
-		ContentType:     "api_dataset_landing_page",
-		CDID:            "ASELECTIONOFNUMBERSANDLETTERS789",
-		DatasetID:       "ASELECTIONOFNUMBERSANDLETTERS101112",
-		Edition:         "another temporary edition",
-		MetaDescription: "another temporary description",
-		ReleaseDate:     time.Time{}.UTC(),
-		Summary:         "another temporary summary",
-		Title:           "another temporary title",
-		Topics:          topics2,
-		Language:        "string",
-		Survey:          "string",
-		CanonicalTopic:  "string",
-		Cancelled:       true,
-		Finalised:       true,
-		Published:       true,
-		DateChanges:     dateChanges,
-		ProvisionalDate: "October-November 2024",
+		var jsonData map[string]interface{}
+		err = json.Unmarshal(fileBytes, &jsonData)
+		if err != nil {
+			log.Fatal(ctx, "Failed to unmarshal JSON for file: "+file.Name(), err)
+		}
+
+		//sort.Sort(sort.Reverse(sort.StringSlice(jsonData)))
+		items[i] = jsonData
 	}
 
-	resourceList[1] = tempResource2
-
-	return resourceList, nil
+	return items, nil
 }
