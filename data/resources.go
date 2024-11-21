82b42d0e6b837a37017186fdcc4aef1a6fc8518c
cookel2 <eleanor.cook@ons.gov.uk>
2024-11-21 15:28:31 +0000

    add release resources, count the files

--- data/resources.go
+++ data/resources.go
@@ -3,8 +3,6 @@ package data
 import (
 	"context"
 	"embed"
-	"fmt"
-
 	"encoding/json"
 
 	"github.com/ONSdigital/dis-search-upstream-stub/models"
@@ -19,17 +17,10 @@ func (r *ResourceStore) GetResources(ctx context.Context, option Options) (*mode
 	logData := log.Data{"option": option}
 	log.Info(ctx, "getting list of resources", logData)
 
-	// get resources count
-	numResources, err := r.getResourcesCount()
+	items, numResources, err := r.populateItems(ctx)
 	if err != nil {
-		log.Error(ctx, "failed to get resources count", err, logData)
-		return nil, err
-	}
-
-	// create and populate resourcesList
-	items := make([]interface{}, numResources)
-	items, err = r.populateItems(ctx, items, numResources)
-	if err != nil {
+		logData["items"] = items
+		logData["numResources"] = numResources
 		log.Error(ctx, "failed to populate resources list", err, logData)
 		return nil, err
 	}
@@ -45,25 +36,25 @@ func (r *ResourceStore) GetResources(ctx context.Context, option Options) (*mode
 	return resources, nil
 }
 
-// getResourcesCount returns the total number of resources stored
-func (r *ResourceStore) getResourcesCount() (int, error) {
-	return 3, nil
-}
-
-func (r *ResourceStore) populateItems(ctx context.Context, items []interface{}, numItems int) ([]interface{}, error) {
+func (r *ResourceStore) populateItems(ctx context.Context) (items []interface{}, numItems int, err error) {
+	logData := log.Data{}
 	// Get a list of JSON files in the embedded 'json_files' directory.
 	files, err := jsonFiles.ReadDir("json_files")
 	if err != nil {
 		log.Fatal(ctx, "Failed to read json_files directory", err)
 	}
+	for range files {
+		numItems++
+	}
+	items = make([]interface{}, numItems)
 
 	// Loop through files, read, and unmarshal each JSON file into Go structs.
 	for i := 0; i < numItems; i++ {
 		file := files[i]
-		fmt.Println("Now reading file: " + file.Name())
+		logData["file"] = file.Name()
 		fileBytes, err := jsonFiles.ReadFile("json_files/" + file.Name())
 		if err != nil {
-			log.Fatal(ctx, "Failed to read file: "+file.Name(), err)
+			log.Fatal(ctx, "Failed to read file", err, logData)
 		}
 
 		var jsonData map[string]interface{}
@@ -75,5 +66,5 @@ func (r *ResourceStore) populateItems(ctx context.Context, items []interface{}, 
 		items[i] = jsonData
 	}
 
-	return items, nil
+	return items, numItems, nil
 }
