1f8e55da4682ca5032cf6dedd29cb02670d24d77
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-06-30 16:03:14 +0100

    Upgrade linter to v2

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 pushd dis-search-upstream-stub
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.5
+  go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.6
   make lint
   npm install -g @redocly/cli
   make validate-specification
